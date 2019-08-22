730187411b7f83434c136058baa1dd4f2997e730
Anthony Moss <asmoss93@gmail.com>
2019-08-21 21:39:23 -0400

    adds validate to email which uses validator npm to validate email provided is actual email address format wise

--- src/models/user.js
+++ src/models/user.js
@@ -12,7 +12,12 @@ const userSchema = new mongoose.Schema({
         required: true,
         trim: true,
         lowercase: true,
-        unique: true
+        unique: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
     },
     password: {
         type: String,
